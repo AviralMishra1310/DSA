3dffa850afb3104ee5edc06b8525f958d6326225
AviralMishra1310 <aviralmishra131005@gmail.com>
2025-12-04 01:39:40 +0530

    Solved LC 516 DP prob

---
+++ LeetCode/DP/LPS.java
@@ -0,0 +1,28 @@
+//LC 516
+import java.util.*;
+
+public class LPS {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        String b = new StringBuilder(a).reverse().toString();
+
+        int m = a.length(), n = b.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(a.charAt(i - 1) == b.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[m][n]);
+    }
+}
+
+
