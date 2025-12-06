23529fecc3d76fb0a949d8b878b2836b01b25567
AviralMishra1310 <aviralmishra131005@gmail.com>
2025-12-07 03:07:50 +0530

    Solved LC 2211

---
+++ LeetCode/Strings/collisions.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class collisions {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int left = 0, right = n - 1;
+
+        while(left < n && directions.charAt(left) == 'L') left++;
+
+        while(right >= 0 && directions.charAt(right) == 'R') right--;
+
+        int collisons = 0;
+
+        for(int i = left; i <= right; i++){
+            char c = directions.charAt(i);
+            if (c == 'L' || c == 'R') collisons++;
+        }
+        return collisons;
+    }
+}
+}
