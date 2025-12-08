60713c23ebf1b7b3aa5d6c08c3b2ce47e99ca977
AviralMishra1310 <aviralmishra131005@gmail.com>
2025-12-08 18:54:58 +0530

    Solved LC 10

---
+++ LeetCode/DP/regularExpresMatch.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class regularExpresMatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();     // input string
+        String p = sc.next();     // pattern
+
+        int n = s.length();
+        int m = p.length();
+
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+
+        for (int j = 2; j <= m; j++) {
+            if (p.charAt(j - 1) == '*')
+                dp[0][j] = dp[0][j - 2];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if (p.charAt(j - 1) == '*') {
+
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+
+        System.out.println(dp[n][m] ? "true" : "false");
+    }
+}
+
+
