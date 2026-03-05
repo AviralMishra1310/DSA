27bc3e2264198fd6e6cf77a3fac5bd89e632a2fa
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-03-05 19:13:25 +0530

    Done a DSA question

---
+++ LeetCode/Strings/LC1758.java
@@ -0,0 +1,15 @@
+class LC1758 {
+    public int minOperations(String s) {
+        int n = s.length();
+        int cnt0 = 0;
+
+        for (int i = 0; i < n; i++) {
+            char expected = (i % 2 == 0 ? '0' : '1');
+            if (s.charAt(i) != expected) {
+                cnt0++;
+            }
+        }
+
+        return Math.min(cnt0, n - cnt0);
+    }
+}
