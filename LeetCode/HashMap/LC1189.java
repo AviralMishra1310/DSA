deac3f68adee98e0e4c25830d6ad4afb099f07c8
AviralMishra1310 <aviralmishra131005@gmail.com>
2026-03-27 23:46:40 +0530

    Done a DSA Question

---
+++ LeetCode/HashMap/LC1189.java
@@ -0,0 +1,17 @@
+public class LC1189 {
+    public int maxNumberOfBalloons(String s) {
+        int Freq[]= new int[26];
+        for(char ch : s.toCharArray()){
+            Freq[ch -'a']++;
+        }
+        return Math.min(
+            Math.min(
+                Math.min(Freq['b'-'a'], Freq['a'-'a']),
+                Math.min(Freq['l'-'a']/2, Freq['o'-'a']/2)
+            ),
+            Freq['n'-'a']
+        );
+
+    }
+}
+
